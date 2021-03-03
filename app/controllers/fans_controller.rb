643e4aab4c2b73828ba9ff99f4e11ac1ad29cb71
Karine Legault <karine.legault.mallette14@gmail.com>
2021-03-02 20:04:46 -0500

    added the show in the controller

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -1,2 +1,13 @@
 class FansController < ApplicationController
+  def show
+    @fans = Fans.find(params[:id])
+    @booking = Booking.new
+  end
+
+  private
+
+  def fans_params
+    params.require(:fans).permit(:name, :description, :price, :user_id, :photo)
+  end
+
 end
