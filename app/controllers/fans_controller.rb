c5a4718d375d3a3ec2fcf22c6addc68c8cfc2452
gad dayan <gadou86@gmail.com>
2021-03-02 20:40:45 -0500

    created_fans works

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -1,2 +1,28 @@
 class FansController < ApplicationController
+
+    def new
+        @fans = Fan.new
+    end
+
+    def edit
+        @fans.update(fans_params)
+        redirect_to fans_path(@fans)
+    end
+
+    def create
+        @fans = Fan.new(fans_params)
+        if @fans.save
+            redirect_to root_path(@fans)
+        else
+            render :new
+        end
+    end
+
+    private
+  
+    def fans_params
+        params.require(:fan).permit(:name, :description)
+    end
+
+
 end
