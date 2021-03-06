663bc0ff0d6378a62ed88b964a80475ef146e3b4
Mirchev Pascal <pascalmirchev2000@gmail.com>
2021-03-06 10:52:31 -0500

    Currently working on booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,11 @@
 class BookingsController < ApplicationController
+  def new
+    @fan = Fan.find( params[:fan_id])
+    @booking = Booking.new
+  end
+
   def create
+  
     @fan = Fan.find(params[:fan_id])
     @booking = Booking.new(booking_params)
     @booking.fan = @fan
@@ -11,7 +17,10 @@ class BookingsController < ApplicationController
   end
 
   private
-
+  
+  # def start_time
+  #   self.my_related_model.start ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+  # end
   def booking_params
     params.require(:booking).permit(:fan_id, :user_id, :start_date, :end_date, :status)
   end
