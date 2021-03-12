6ff04917c68b5735165c93310c409a0e7f98ff87
gad dayan <gadou86@gmail.com>
2021-03-12 15:22:58 -0500

    Added weather on nav bar and date

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,29 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=montreal&appid=2c0c06999bf536f4bcb205cd7d15c68f&units=metric'
+
+const city = document.querySelector("#city");
+const date = document.querySelector("#date");
+const description = document.querySelector("#description");
+const weather = document.querySelector("#weather");
+const icon = document.querySelector("#icon");
+
+
+fetch(url)
+.then(response  => response.json())
+.then((data) => {
+    console.log(data)
+    // city.innerText = data.name
+    const today = new Date()
+    const localOffset = data.timezone + today.getTimezoneOffset() * 60;
+    const localDate = new Date(today.setUTCSeconds(localOffset));
+    const options = { weekday: 'long',  month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    const formattedDate = localDate.toLocaleDateString('en-Us', options)
+    date.innerText = formattedDate;
+    console.log(localDate)
+    // description.innerText = data.weather[0].description
+    weather.innerText = `${Math.round(data.main.temp)}°C`;
+    icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+})
